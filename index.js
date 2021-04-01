642297fd0c8b6d01d07e66326abba12680d7f4b8
GIDEON <9434592+jjdharmaraj@users.noreply.github.com>
2021-04-01 10:06:00 -0500

    setup the basics for extracting audio from a video file

--- index.js
+++ index.js
@@ -13,10 +13,24 @@ const subscriptionKey = process.env.AZURE_SPEECH_SUBSCRIPTION_KEY;
 //https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/regions
 const serviceRegion = process.env.AZURE_SPEECH_SERVICE_REGION;
 
-function envTest() {
-  console.log(
-    `subscriptionKey: ${subscriptionKey}, serviceRegion: ${serviceRegion}`
-  );
-}
+let rawConfig = fs.readFileSync("config.json");
+let config = JSON.parse(rawConfig);
 
-envTest();
+//This is the video you want to use to generate subtitles for.
+let videoFile = config.videoFile;
+//This audio file is generated before sending to Azure AI.
+let audioFile = config.audioFile;
+
+extractAudio({
+  input: videoFile,
+  output: audioFile,
+  transform: (cmd) => {
+    cmd.audioChannels(1).audioFrequency(16000);
+  },
+})
+  .then(() => {
+    console.log(`Extract Audio Done`);
+  })
+  .catch((e) => {
+    console.log(`Extract Audio Error: ${e}`);
+  });
